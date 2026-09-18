2cc66a5af3b2db5e81563da5cf1658b60eff6bc9
Narain Sivakumar <narainsivakumar05@gmail.com>
2026-09-18 15:35:57 +0530

    Update sample.py
    This is my first python coding in github

--- sample.py
+++ sample.py
@@ -1,3 +1,12 @@
 # PYTHON
-CONTAIN ALL THE SIMPLE CODES IN PYTHON LANGUAGE
+CONTAINS ALL THE SIMPLE CODES IN PYTHON LANGUAGE
 coding basics
+python coding for sum of two numbers:
+num1 = int(input("Enter first number:"))
+num2 = int(input("Enter second number:"))
+sum_result = num1 + num2
+print(f"The sum of {num1} and {num2} is {sum_result}")
+OUTPUT FOR THE ABOVE CODING:
+Enter first number:10
+Enter second number:20
+The sum of 10 and 20 is 30
